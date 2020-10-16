2d55a934055c0e097d1b3749435ee4c7bf7531b0
Yana <kolodyazhnaya19@yandex.ru>
2020-10-16 19:16:02 +0300

    change_model

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let arr = [1, 1, 0,
            0, 0, 2,
            2, 1, 0];
 
-for (let i = 0; i < 9; i++ ) {
-    changeColor(i, arr[i]);
-}
+function visualisation() {
+    for (let i = 0; i < 9; i++ ) {
+        changeColor(i, arr[i]);
+    }
+} 
 
 function changeColor(i, color) {
     if (color == 1) {
@@ -15,6 +17,10 @@ function changeColor(i, color) {
         document.getElementById(i).classList.remove("color_pink");
         document.getElementById(i).classList.remove("color_green");
     }
+}
+
+function changeModel(i, color) {
+    arr[i] = color;
 }
 
 
